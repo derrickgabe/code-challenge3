7891fd4246f35c7e668bcb87bd9543978275293b
Derrick <derrickg844@gmail.com>
2025-07-20 13:20:55 +0300

    update

--- smart goal planner/vite.config.js
+++ smart goal planner/vite.config.js
@@ -3,15 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/smart-goal-planner/', // Match your repo name exactly
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  base: '/code-challenge3/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+
 })
