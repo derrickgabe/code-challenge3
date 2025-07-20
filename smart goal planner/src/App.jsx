228381e5237466c541f321c104e01ac8fd4a5ace
Derrick <derrickg844@gmail.com>
2025-07-20 14:10:17 +0300

    update

--- smart goal planner/src/App.jsx
+++ smart goal planner/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     }
   }, []);
 
-  // ... rest of your app
+  
 }
 
 
--- smart goal planner/src/main.jsx
+++ smart goal planner/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Router basename="/code-challenge3">
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  </Router>
+);
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
